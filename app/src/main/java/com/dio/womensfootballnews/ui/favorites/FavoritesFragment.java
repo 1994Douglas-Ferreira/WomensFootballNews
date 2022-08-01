a6952d7ad2754ebb4c5fc3e432f725e85e561842
Ferreira <douglas.ferreira@hotmail.com>
2022-08-01 16:26:24 -0300

    Refactoring class and layouts

--- app/src/main/java/com/dio/womensfootballnews/ui/favorites/FavoritesFragment.java
+++ app/src/main/java/com/dio/womensfootballnews/ui/favorites/FavoritesFragment.java
@@ -1,4 +1,4 @@
-package com.dio.womensfootballnews.ui.home;
+package com.dio.womensfootballnews.ui.favorites;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.dio.womensfootballnews.databinding.FragmentHomeBinding;
+import com.dio.womensfootballnews.databinding.FragmentFavoritesBinding;
 
-public class HomeFragment extends Fragment {
+public class FavoritesFragment extends Fragment {
 
-    private FragmentHomeBinding binding;
+    private FragmentFavoritesBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+        FavoritesViewModel dashboardViewModel =
+                new ViewModelProvider(this).get(FavoritesViewModel.class);
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        binding = FragmentFavoritesBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textView = binding.textFavorites;
+        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
--- app/src/main/java/com/dio/womensfootballnews/ui/news/NewsFragment.java
+++ app/src/main/java/com/dio/womensfootballnews/ui/news/NewsFragment.java
@@ -1,4 +1,4 @@
-package com.dio.womensfootballnews.ui.dashboard;
+package com.dio.womensfootballnews.ui.news;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.dio.womensfootballnews.databinding.FragmentDashboardBinding;
+import com.dio.womensfootballnews.databinding.FragmentNewsBinding;
 
-public class DashboardFragment extends Fragment {
+public class NewsFragment extends Fragment {
 
-    private FragmentDashboardBinding binding;
+    private FragmentNewsBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
+        NewsViewModel homeViewModel =
+                new ViewModelProvider(this).get(NewsViewModel.class);
 
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        binding = FragmentNewsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textView = binding.textNews;
+        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
